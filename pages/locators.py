37c03b7c1b59a2b0a2d7569b7c0859638e3bb94a
Karina <karina.suzd@hotmail.com>
2020-08-23 11:04:09 +0300

    Product Page tests added

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,12 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "form#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "form#register_form")
+
+
+class ProductPageLocators:
+    ADDTOCART_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    SUCCESS_INFO = (By.CSS_SELECTOR, "div.alert-success")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main > h1")
+    INFO_NAME = (By.CSS_SELECTOR, "div.alert-success strong")
+    PRICE = (By.CSS_SELECTOR, "p.price_color")
+    INFO_PRICE = (By.CSS_SELECTOR, "div.alert-info strong")
---
+++ pages/product_page.py
@@ -0,0 +1,38 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def should_be_added_to_cart(self):
+        self.should_be_addtocart_button()
+        self.should_be_successful_added()
+        self.should_be_correct_name()
+        self.should_be_correct_price()
+
+    def should_be_successful_added(self):
+        button = self.browser.find_element(
+            *ProductPageLocators.ADDTOCART_BUTTON)
+        button.click()
+        self.solve_quiz_and_get_code()
+        assert self.is_element_present(
+            *ProductPageLocators.SUCCESS_INFO
+        ), "The product was not added"
+
+    def should_be_addtocart_button(self):
+        assert self.is_element_present(
+            *ProductPageLocators.ADDTOCART_BUTTON
+        ), "Add to cart button is not presented"
+
+    def should_be_correct_name(self):
+        name = self.browser.find_element(
+            *ProductPageLocators.PRODUCT_NAME).text
+        info = self.browser.find_element(
+            *ProductPageLocators.INFO_NAME).text
+        assert name == info, "Another product was added"
+
+    def should_be_correct_price(self):
+        price = self.browser.find_element(
+            *ProductPageLocators.PRICE).text
+        info = self.browser.find_element(
+            *ProductPageLocators.INFO_PRICE).text
+        assert price == info, "Another product was displayed"
