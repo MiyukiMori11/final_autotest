ea0e108ec4e987e5f3bf5775af1d509084b56dc4
Karina <karina.suzd@hotmail.com>
2020-09-22 16:01:37 +0300

    Negative tests added, code optimized

--- pages/product_page.py
+++ pages/product_page.py
@@ -9,10 +9,13 @@ class ProductPage(BasePage):
         self.should_be_correct_name()
         self.should_be_correct_price()
 
-    def should_be_successful_added(self):
+    def should_be_clicked_addtocart_button(self):
         button = self.browser.find_element(
             *ProductPageLocators.ADDTOCART_BUTTON)
         button.click()
+
+    def should_be_successful_added(self):
+        self.should_be_clicked_addtocart_button()
         self.solve_quiz_and_get_code()
         assert self.is_element_present(
             *ProductPageLocators.SUCCESS_INFO
@@ -36,3 +39,11 @@ class ProductPage(BasePage):
         info = self.browser.find_element(
             *ProductPageLocators.INFO_PRICE).text
         assert price == info, "Another product was displayed"
+
+    def cant_see_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_INFO), \
+            "Success info was presented"
+
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_INFO), \
+            "The success message didn't disappear"
--- test_product_page.py
+++ test_product_page.py
@@ -11,3 +11,32 @@ def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_added_to_cart()
+
+
+@pytest.mark.xfail()
+def test_guest_cant_see_success_message_after_adding_product_to_basket(
+        browser):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue' \
+           f'/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_clicked_addtocart_button()
+    page.cant_see_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue' \
+           f'/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.cant_see_success_message()
+
+
+@pytest.mark.xfail()
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue' \
+           f'/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_clicked_addtocart_button()
+    page.should_disappear_success_message()
