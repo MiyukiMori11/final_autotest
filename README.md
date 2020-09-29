0cab4e81812f302a292bc5e5beed0e2c1a4e1119
Karina <karina.suzd@hotmail.com>
2020-09-29 14:55:47 +0300

    Readme updated, need_review mark added, code optimized

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# final_autotest
+# Set of tests for the final project of an Automatic Testing course
+
+**Site for testing:** http://selenium1py.pythonanywhere.com
+
+**Testing areas:**
+Main page,
+Basket (cart) page,
+Login page,
+Product page
+
+To change browser language, add the flag `--language` to your pytest command
+
+Example: `pytest --language="fr"`
+
--- test_product_page.py
+++ test_product_page.py
@@ -17,9 +17,13 @@ class TestUserAddToBasketFromProductPage:
         page.register_new_user(email, password)
         page.should_be_authorized_user()
 
-    @pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
-                                      pytest.param(7, marks=pytest.mark.xfail),
-                                      8, 9])
+    @pytest.mark.need_review
+    @pytest.mark.parametrize('link', [0,
+                                      # 1, 2, 3, 4, 5, 6,
+                                      # pytest.param(7,
+                                      #              marks=pytest.mark.xfail),
+                                      # 8, 9
+                                      ])
     def test_user_can_add_product_to_basket(self, browser, link):
         link = f'http://selenium1py.pythonanywhere.com/catalogue' \
                f'/coders-at-work_207/?promo=offer{link} '
@@ -35,9 +39,12 @@ class TestUserAddToBasketFromProductPage:
         page.cant_see_success_message()
 
 
-@pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
-                                  pytest.param(7, marks=pytest.mark.xfail),
-                                  8, 9])
+@pytest.mark.need_review
+@pytest.mark.parametrize('link', [0,
+                                  # 1, 2, 3, 4, 5, 6,
+                                  # pytest.param(7, marks=pytest.mark.xfail),
+                                  # 8, 9
+                                  ])
 def test_guest_can_add_product_to_basket(browser, link):
     link = f'http://selenium1py.pythonanywhere.com/catalogue' \
            f'/coders-at-work_207/?promo=offer{link} '
@@ -83,6 +90,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/" \
            "the-city-and-the-stars_95/"
@@ -91,6 +99,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/" \
            "the-city-and-the-stars_95/"
